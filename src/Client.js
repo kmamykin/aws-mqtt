22dcff252431b879baacddc929596a86e12c4041
Kliment Mamykin <kmamyk@gmail.com>
2018-07-11 10:24:48 -0400

    Moved processOptions function inside Client.js

--- src/Client.js
+++ src/Client.js
@@ -1,11 +1,35 @@
 import MqttClient from 'mqtt/lib/client'
-import processOptions from './processOptions'
 import createWebSocketStream from './createWebSocketStream'
+
+// Not all MQTT options will work with AWS, here we handpick options that are safe to pass on to MQTT
+const processOptions = (options = {}) => {
+  return {
+    WebSocket: options.WebSocket || window.WebSocket,
+    aws: {
+      region: options.region,
+      endpoint: options.endpoint,
+      credentials: options.credentials
+    },
+    mqttOptions: {
+      clientId: options.clientId || 'mqtt-client-' + (Math.floor((Math.random() * 100000) + 1)),
+      // protocolId: options.protocolId || 'MQTT',
+      // protocolVersion: options.protocolVersion || 4,
+      //reconnectPeriod: options.reconnectPeriod || 2 * 1000,
+      //connectTimeout: options.connectTimeout || 10 * 1000,
+      connectTimeout: options.connectTimeout || 5 * 1000,
+      reconnectPeriod: options.reconnectPeriod || 10 * 1000,
+      clean: options.clean || true, // need to re-subscribe after offline/disconnect,
+      // queueQoSZero: options.queueQoSZero || true,
+      will: options.will || {}
+    }
+  }
+}
 
 class Client extends MqttClient {
   constructor(options) {
-    const {WebSocket, aws, mqtt} = processOptions(options)
-    super(() => createWebSocketStream(WebSocket, aws), mqtt)
+    const { WebSocket, aws, mqttOptions } = processOptions(options)
+    const streamBuilder = () => createWebSocketStream(WebSocket, aws)
+    super(streamBuilder, mqttOptions)
   }
 }
 
