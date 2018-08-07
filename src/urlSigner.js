21b4e98efe0a865a1388641ec0e9df45a4ced47d
Kliment Mamykin <kmamyk@gmail.com>
2018-08-07 17:06:13 -0400

    Added jest for testing

--- src/urlSigner.js
+++ src/urlSigner.js
@@ -1,14 +1,14 @@
 import v4 from 'aws-signature-v4'
 import crypto from 'crypto'
-// TODO: look into ./~/aws-sdk/lib/signers/v4.js
 
 export const sign = ({ credentials, endpoint, region, expires }) => {
+  const payload = crypto.createHash('sha256').update('', 'utf8').digest('hex')
   return v4.createPresignedURL(
     'GET',
     endpoint,
     '/mqtt',
     'iotdevicegateway',
-    crypto.createHash('sha256').update('', 'utf8').digest('hex'),
+    payload,
     {
       key: credentials.accessKeyId,
       secret: credentials.secretAccessKey,
